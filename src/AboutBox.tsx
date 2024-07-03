55c67168ceeb81a7761c2d765b5f944cb1c9985f
Bas van Driel <contact@basvandriel.nl>
2024-07-03 09:57:51 +0200

    work on abouts

---
+++ src/AboutBox.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import ArrowTopRightOnSquareIcon from "@heroicons/react/24/outline/ArrowTopRightOnSquareIcon";
+import EnvelopeIcon from "@heroicons/react/24/outline/EnvelopeIcon";
+
+const EMAIL = "contact@basvandriel.nl";
+
+const AboutBox = () => {
+  return (
+    <div className="bg-white rounded-xl p-1">
+      <div className="p-4">
+        <p className="font-sans font-medium">About me</p>
+        <p className="font-sans text-sm align-middle items-center flex gap-x-2">
+          Technology and user experience
+        </p>
+      </div>
+
+      <div className="p-4 bg-gray-100 rounded-xl text-sm">
+        <p className="text-sm text-gray-600">
+          I am passionate about using technology to solve real-world problems. I
+          am always looking for new ways to improve the user experience and make
+          software more accessible to everyone. I am also a strong advocate for
+          open source software and I am always willing to contribute to the
+          community.
+        </p>
+
+        <div className="my-4 border-b border-gray-300"></div>
+
+        <div className="flex flex-wrap gap-4">
+          <div className="inline-flex font-medium gap-x-2 items-center">
+            <a className="inline-flex font-medium gap-x-2 items-center" href="#">
+              <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+              https://basvandriel.nl
+            </a>
+          </div>
+          <div className="inline-flex font-medium gap-x-2 items-center">
+            <a
+              href={`mailto:${EMAIL}`}
+              className="inline-flex font-medium gap-x-2 items-center"
+            >
+              <EnvelopeIcon className="h-4 w-4" />
+              {EMAIL}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AboutBox;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
-import { EnvelopeIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import AboutBox from "./AboutBox";
 
 
 const ROLE = "Senior Python Developer, DevOps Engineer";
@@ -83,40 +83,7 @@ function App() {
 
         {/* TODO experience thing */}
         <div className="col-span-2"></div>
-
-        {/* box */}
-        <div className="bg-white rounded-xl p-1">
-          <div className="p-4">
-            <p className="font-sans font-medium">About me</p>
-            <p className="font-sans text-sm align-middle items-center flex gap-x-2">
-              Passionate about technology, user experience and guitars.
-            </p>
-          </div>
-
-          <div className="p-4 bg-gray-100 rounded-xl text-sm">
-            <p className="text-sm text-gray-600">
-              I am passionate about using technology to solve real-world
-              problems. I am always looking for new ways to improve the user
-              experience and make software more accessible to everyone. I am
-              also a strong advocate for open source software and I am always
-              willing to contribute to the community.
-            </p>
-
-            <div className="my-4 border-b border-gray-300"></div>
-
-            <div className="flex flex-wrap gap-4">
-              <div className="inline-flex font-medium gap-x-2 items-center">
-                <EnvelopeIcon className="h-4 w-4" />
-                contact@basvandriel.nl
-              </div>
-
-              <div className="inline-flex font-medium gap-x-2 items-center">
-                <ArrowTopRightOnSquareIcon className="h-4 w-4" />
-                https://basvandriel.nl
-              </div>
-            </div>
-          </div>
-        </div>
+        <AboutBox />
       </div>
     </div>
   );
