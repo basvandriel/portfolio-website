9e0998ca28dc6d015781fc438e6e03431e4c9503
Bas van Driel <contact@basvandriel.nl>
2024-08-01 21:22:14 +0200

    rm data

--- src/CV/index.tsx
+++ src/CV/index.tsx
@@ -4,18 +4,39 @@ import { Document, Page, View, Text } from "@react-pdf/renderer";
 import tw from "./tailwind";
 import ExperienceListing from "./ExperienceListing";
 
-import experience from '../translateable_experience_data'
 import { TAILWIND_SM_FONTSIZE_IN_PT } from "./constants";
 import Sidebar from "./Sidebar";
 import Header from "./CVHeader";
 import EducationSection from "./EducationSection";
 import SkillsSection from "./SkillsSection";
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { Experience } from "../translateable_experience";
 
 const HEADER_FONT_SIZE = 16;
 
 const WorkExperienceSection = () => {
-  const { t } = useTranslation()
+  const [data, setData] = useState<Experience[]>()
+  const { t, i18n} = useTranslation()
+
+  useEffect(() => {
+    (async () => {
+      const experienceFileName = `experience_${i18n.language}.json`;
+
+      const response = await fetch("/data/" + experienceFileName, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      let json = await response.json();
+
+      setData(json);
+    })();
+  }, [i18n.language]);
+
+
+  if (!data) return null
   return (
     <>
       <Text
@@ -31,7 +52,8 @@ const WorkExperienceSection = () => {
       </Text>
 
       <View style={tw("gap-8 mt-4")}>
-        {experience.map((v, i) => {
+        {data.map((v, i) => {
+          // return <Text>hi</Text>
           return <ExperienceListing {...v} key={i} />;
         })}
       </View>
