a4fcdd4dc327adba48be30a57784f8bddf7dadfd
Bas van Driel <contact@basvandriel.nl>
2024-08-01 20:44:17 +0200

    work

--- src/ExperienceBox.tsx
+++ src/ExperienceBox.tsx
@@ -1,15 +1,44 @@
 import { useTranslation } from "react-i18next";
 import ExperienceListing from "./ExperienceListing";
 
-import data from './translateable_experience_data'
+// import data from './translateable_experience_data'
+import { useEffect, useState } from "react";
+import { Experience } from "./translateable_experience";
 
 const ExperienceBox = () => {
-  const { t  } = useTranslation()
+  const [data, setData] = useState<Experience[]>([])
+
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    if (data.length > 0) return
+
+    (async () => {
+      const x = await fetch('/data/experience.json', {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      })
+      let json = await x.json()
+      // json['start'] = new Date(json['start'])
+      // json['end'] = new Date(json['end'])
+
+      setData(json)
+    })()
+  })
+  
+
+  if (!data) return null
+
+  console.log(data)
+
   return (
     <div className="bg-white rounded-xl mt-4 lg:mt-0">
       <div className="p-1">
         <div className="p-4 font-medium text-base">{t('experience')}</div>
         {data.map((v, i) => {
+          // return <p key={i}>hi</p>
           return <ExperienceListing {...v} key={i} />;
         })}
       </div>
--- src/translateable_experience.ts
+++ src/translateable_experience.ts
@@ -10,3 +10,17 @@ export default interface TranslateableExperience {
     start: Date
     end?: Date
 }
+
+
+
+export interface Experience {
+    title: string,
+    company: string,
+    company_city: string,
+    company_country: string,
+    description: string,
+    tags: string[]
+    company_logo?: string
+    start: Date
+    end?: Date
+}
