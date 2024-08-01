0a0443bf327bd67eb20838a8523e526289ca76cb
Bas van Driel <contact@basvandriel.nl>
2024-08-01 21:23:40 +0200

    rm

--- src/experience.ts
+++ src/experience.ts
@@ -1,18 +1,3 @@
-
-export default interface TranslateableExperience {
-    title: string,
-    company: string,
-    company_city_key: string,
-    company_country_key: string,
-    description_key: string,
-    tags: string[]
-    company_logo?: string
-    start: Date
-    end?: Date
-}
-
-
-
 export interface Experience {
     title: string,
     company: string,
