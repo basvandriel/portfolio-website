9370fa79a1c4e7cb37aa54e2c206cfcf42c93685
Bas van Driel <contact@basvandriel.nl>
2024-12-07 16:12:53 +0100

    tryout

---
+++ scripts/build-cv-pdf/build-script.js
@@ -0,0 +1,98 @@
+#!/usr/bin/node
+
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+const webpack = require('webpack')
+
+let webpack_config = {
+    mode: "production",
+    entry: "./src/generatePDF.tsx",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname),
+    },
+    // We need this for a warning on tailwindcss 
+    ignoreWarnings: [
+      /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/,
+    ],
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: "./src/CV/fonts", to: "fonts" }],
+      }),
+    ],
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader'},
+            { loader: 'css-loader'},
+            {
+              loader: require.resolve("postcss-loader"),
+              options: {
+                postcssOptions: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebook/create-react-app/issues/2677
+                  ident: "postcss",
+                  config: false,
+                  plugins: [
+                    "tailwindcss",
+                    "postcss-flexbugs-fixes",
+                    [
+                      "postcss-preset-env",
+                      {
+                        autoprefixer: {
+                          flexbox: "no-2009",
+                        },
+                        stage: 3,
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+        {
+          test: /\.(j|t)sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              compact: true,
+              presets: [
+                "@babel/preset-typescript",
+                [
+                  "@babel/preset-env",
+                  {
+                    modules: false,
+                  },
+                ],
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+    },
+    target: "node",
+  }
+
+webpack(webpack_config, (err, stats) => {})
