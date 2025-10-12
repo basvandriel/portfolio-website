b1606678e408cf141e6cae985e9757cc2fbe5870
Bas <contact@basvandriel.nl>
2025-10-12 14:26:52 +0200

    work

--- src/Redesign.tsx
+++ src/Redesign.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import me from "./bas.png";
 import EducationSection from "./RedesignedEducationSection";
 import WorkSection from "./RedesignedWorkSection";
--- src/components/CVLink.tsx
+++ src/components/CVLink.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import ReactGA from "react-ga4";
 
 type Props = {
